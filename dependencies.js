86748e198c22d4b9acea38c2fe49a8598f936634
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-05-06 11:19:59 -0700

    Add more infrastructure, plus first script

--- dependencies.js
+++ dependencies.js
@@ -1,2 +1,7 @@
 const fs = require('fs');
-fs.readFileSync('1-percent.json', 'utf8')
+const sh = require('shelljs');
+const packages = JSON.parse(fs.readFileSync('1-percent.json', 'utf8'))
+for (let p in packages) {
+    console.log("==== " + p + " =====");
+    console.log(sh.grep('react', ["/home/nathansa/DefinitelyTyped/types/" + p + "/*.ts"]).stdout)
+}
