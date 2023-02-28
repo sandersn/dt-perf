23fecd38a4385e90856300a7d056c3af5ed80652
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-02-27 16:54:02 -0800

    Switch to @stdlib/stats-wilcoxon
    Recommended by @jakebailey

--- perf/compare.js
+++ perf/compare.js
@@ -4,6 +4,7 @@
  */
 import fs from 'fs'
 import d3 from 'd3-format'
+import wilcoxon from '@stdlib/stats-wilcoxon'
 const pct = d3.format(".2%")
 const beforedata = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'))
 const afterdata = JSON.parse(fs.readFileSync(process.argv[3], 'utf8'))
@@ -17,9 +18,15 @@ for (const k of Object.keys(beforedata)) {
 const beforetotal = {}
 /** @type {Record<string, number>} */
 const aftertotal = {}
+/** @type {Record<string, number[]>} */
+const befores = {}
+/** @type {Record<string, number[]>} */
+const afters = {}
 for (const [before,after] of Object.values(both)) {
     for (const k of Object.keys(before)) {
-        if (k in after) {
+        if (k in after && after[k] != null && before[k] != null) {
+            ;(befores[k] = (befores[k] ?? [])).push(before[k])
+            ;(afters[k] = (afters[k] ?? [])).push(after[k])
             beforetotal[k] = (beforetotal[k] ?? 0) + before[k]
             aftertotal[k] = (aftertotal[k] ?? 0) + after[k]
         }
@@ -27,29 +34,22 @@ for (const [before,after] of Object.values(both)) {
 }
 for (const k of Object.keys(beforetotal)) {
     if (k === 'name') continue
-    console.log(`${k}:\t\t${beforetotal[k]}\t${aftertotal[k]}\t${pct((aftertotal[k] - beforetotal[k]) / beforetotal[k])}\t${monteCarlo(both, k)}`)
+    console.log(`${k}:\t\t${pct((aftertotal[k] - beforetotal[k]) / beforetotal[k])}\t${wc(befores[k], afters[k])}`)
+    // console.log(`${k}:\t\t${beforetotal[k]}\t${aftertotal[k]}\t${pct((aftertotal[k] - beforetotal[k]) / beforetotal[k])}\t${wc(befores[k], afters[k])}`)
 }
 
 /**
- * Note: I compare percent differences between runs, so small projects may have more weight than large projects.
- * I need to do some more thinking or reading to decide which is right here.
- * @param {Record<string, [Perf, Perf]>} both
- * @param {string} k
- * @param {number} [n=100]
+ * @param {number[]} before
+ * @param {number[]} after
  */
-function monteCarlo(both, k, n = 100) {
-    const values = Object.values(both)
-    const original = values.map(([before, after]) => (after[k] - before[k]) / before[k]).reduce((a, b) => a + b, 0) // not scaled percentage per-project, so large projects have more weight
-    // (percentage scaling would give small projects more weight)
-    // The null hypothesis is that randomly shuffling which result is from before/after would produce at least as large a difference
-    let nullHypothesis = 0
-    for (let i = 0; i < n; i++) {
-        const shuffled = values.map(([before, after]) => (Math.random() > 0.5 ? 1 : -1) * ((after[k] - before[k]) / before[k])).reduce((a, b) => a + b, 0)
-        if (Math.abs(shuffled) > Math.abs(original)) {
-            nullHypothesis++
-        }
+function wc(before, after) {
+    if (before.every((v, i) => v === after[i]))
+        return '-'
+    const result = wilcoxon(before, after)
+    if (result.rejected) {
+        return result.pValue < 0.01 ? '**' : result.pValue < 0.05 ? '*' : '?'
     }
-    return nullHypothesis <= (n / 100) ? "**"
-        : nullHypothesis <= (n / 20) ? "*"
-        : `(${nullHypothesis} / ${n})`
+    else {
+        return ''
+    }
 }
